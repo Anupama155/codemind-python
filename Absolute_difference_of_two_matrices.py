7d295ff5627d20eb0affa3355c1db4ba9e5804ec
Anupama155 <119721073+Anupama155@users.noreply.github.com>
2023-11-03 15:48:15 +0530

    r = int(input()) mat1=[] mat2=[] for a in range(r):     b=list(map(int,input().split()))     mat1.append(b) for c in range(r):     d=list(map(int,input().split()))     mat2.append(d) z=[] for i in range(r):     e=[]     for j in range(r):         s=abs(mat1[i][j]-mat2[i][j])         e.append(s)     z.append(e) for x in z:     print(*x)

---
+++ Absolute_difference_of_two_matrices.py
@@ -0,0 +1,18 @@
+r = int(input())
+mat1=[]
+mat2=[]
+for a in range(r):
+    b=list(map(int,input().split()))
+    mat1.append(b)
+for c in range(r):
+    d=list(map(int,input().split()))
+    mat2.append(d)
+z=[]
+for i in range(r):
+    e=[]
+    for j in range(r):
+        s=abs(mat1[i][j]-mat2[i][j])
+        e.append(s)
+    z.append(e)
+for x in z:
+    print(*x)
